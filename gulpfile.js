44e48e6b96e7d894b85bfd33b8ee02d2a69dfe22
danica cahuin <dcahuin@gmail.com>
2022-01-11 19:49:05 -0300

    proyecto

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/* importando el modelo instalado, cuando veamos las {} significa
+que se exportando mas de una función */
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+
+// compilar el codigo de Sass
+function compilarCss(done) {
+  /*
+  1. acceder a la ruta del archivo sass
+  2. necesitamos compilar ese archivo sass
+  3. guardamos el archivo .css en una carpeta
+  */
+  // src es la función que me nos ayudar a indentificar la carpeta que contiene el archivo sass
+  // dest es la funcion que nos va guardar el archivo compilado
+  src('src/scss/styles.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('build/css'));
+  /**
+   * .pipe(sass({outputStyle: 'compressed'})) nos entrega una hoja minificada de css
+   * .pipe(sass({outputStyle: 'expanded'})) nos entrega una hoja identada mas facil de leer
+   * 
+   * Autoprefixer es un plugin de PostCss que nos permite analizar nuestro css y garantizar la compatibilidad
+   * con navegadores mas antiguos
+   */
+
+  done();
+}
+
+
+
+function watchChange() {
+  // watch esta atento a lo que pase en el archivo style.scss
+  // y si hay cambios vuelve a llamar a la funcion css
+  // watch('src/scss/styles.scss', compilarCss)
+  watch('src/scss/**/*.scss',compilarCss)
+}
+exports.default = series(compilarCss, watchChange); // que se llama solo con el comando y sin especificar la tarea
+// exports.compilarCss = compilarCss;
+// exports.watchChange = watchChange;
